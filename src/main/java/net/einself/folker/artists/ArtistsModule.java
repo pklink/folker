cae9e4fd655032916115a573bee3e6672d439d9c
Pierre Klink <dev@einself.net>
2025-04-24 22:54:23 +0200

    Revert "feat(artists): add Artist domain, HTTP endpoints, and tests"
    This reverts commit 04f5e931991ccfede330b7c3e751d47ff21001dd.

--- src/main/java/net/einself/folker/artists/ArtistsModule.java
+++
@@ -1,7 +0,0 @@
-package net.einself.folker.artists;
-
-import org.springframework.modulith.Modulith;
-
-@Modulith
-public class ArtistsModule {
-}
--- src/main/java/net/einself/folker/artists/domain/ArtistRepository.java
+++
@@ -1,9 +0,0 @@
-package net.einself.folker.artists.domain;
-
-import io.micronaut.data.mongodb.annotation.MongoRepository;
-import io.micronaut.data.repository.CrudRepository;
-import org.bson.types.ObjectId;
-
-@MongoRepository
-public interface ArtistRepository extends CrudRepository<Artist, ObjectId> {
-}
