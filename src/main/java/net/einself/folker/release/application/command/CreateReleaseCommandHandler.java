e287003d371e76f1a3e4740bab3a8006ef850a2b
Pierre Klink <dev@einself.net>
2025-08-03 22:23:49 +0200

    feat: enhance CreateReleaseCommandHandler to save releases and publish events

--- src/main/java/net/einself/folker/release/application/command/CreateReleaseCommandHandler.java
+++ src/main/java/net/einself/folker/release/application/command/CreateReleaseCommandHandler.java
@@ -1,25 +1,31 @@
 package net.einself.folker.release.application.command;
 
-import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
+import net.einself.folker.core.application.EventPublisher;
 import net.einself.folker.core.application.command.CommandHandler;
 import net.einself.folker.core.application.command.CommandResult;
-import net.einself.folker.release.application.ReleaseService;
 import net.einself.folker.release.domain.Release;
+import net.einself.folker.release.domain.ReleaseCreatedEvent;
+import net.einself.folker.release.domain.ReleaseRepository;
 
 @Singleton
 public class CreateReleaseCommandHandler implements CommandHandler<CreateReleaseCommand, Release> {
 
-    private final ReleaseService releaseService;
+    private final ReleaseRepository releaseRepository;
+    private final EventPublisher eventPublisher;
 
-    @Inject
-    public CreateReleaseCommandHandler(ReleaseService releaseService) {
-        this.releaseService = releaseService;
+    public CreateReleaseCommandHandler(ReleaseRepository releaseRepository, EventPublisher eventPublisher) {
+        this.releaseRepository = releaseRepository;
+        this.eventPublisher = eventPublisher;
     }
 
     @Override
     public CommandResult<Release> handle(CreateReleaseCommand command) {
-        Release release = releaseService.create(command.release());
+        Release release = releaseRepository.save(command.release());
+
+        ReleaseCreatedEvent event = new ReleaseCreatedEvent(release);
+        eventPublisher.publish(event);
+
         return CommandResult.ok(release);
     }
 
